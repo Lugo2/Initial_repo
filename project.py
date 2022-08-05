f14f0e3207a3b8538cd040b8ea0afb5b00ed07d4
Mario Lugo <mlugo23699@gmail.com>
2022-08-05 16:18:25 -0700

    adding modified project file

---
+++ project.py
@@ -0,0 +1,24 @@
+city_list=['Tijuana','Lima','Amsterdam','Cairo','Bangkok','Wagga Wagga']
+Tijuana_eats=['Sabor a Mi','Tacos el Gordo','Villa Marina Tijuana','Da Salvatore','Saketori-Ya']
+Tijuana_transportation=['taxi','bus','car rental','']
+Tijuana_entertainment=['','','','','']
+
+Lima_eats=['','','','','']
+Lima_transportation=['','','','']
+Lima_entertainment=['','','','','']
+
+Amsterdam_eats=['','','','','']
+Amsterdam_transportation=['bicycle','','','']
+Amsterdam_entertainment=['','','','','']
+
+Cairo_eats=['','','','','']
+Cairo_transportation=['','','','']
+Cairo_entertainment=['','','','','']
+
+Bangkok_eats=['','','','','']
+Bangkok_transportation=['','','','']
+Bangkok_entertainment=['','','','','']
+
+Wagga_Wagga_eats=['','','','','']
+Wagga_Wagga_transportation=['','','','']
+Wagga_Wagga_entertainment=['','','','','']
