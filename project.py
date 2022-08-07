1fea95739bee0a8b2146301340442325d095af7b
Mario Lugo <mlugo23699@gmail.com>
2022-08-06 18:02:49 -0700

    adding started/modified project

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 city_list=['Tijuana','Lima','Amsterdam','Cairo','Bangkok','Wagga Wagga']
 Tijuana_eats=['Sabor a Mi','Tacos el Gordo','Villa Marina Tijuana','Da Salvatore','Saketori-Ya']
-Tijuana_transportation=['taxi','bus','car rental','']
+Tijuana_transportation=['taxi','bus','car rental','bicycle']
 Tijuana_entertainment=['','','','','']
 
 Lima_eats=['','','','','']
@@ -22,3 +22,24 @@ Bangkok_entertainment=['','','','','']
 Wagga_Wagga_eats=['','','','','']
 Wagga_Wagga_transportation=['','','','']
 Wagga_Wagga_entertainment=['','','','','']
+
+print('Hello, I am your firendly neighborhood day trip generator! Today we will be planning your next day trip. Lets go!')
+import random
+
+destination=random.choice(city_list)
+input(f'I have chosen {destination} for you, is this where you would like to go? y/n')
+
+answer=input
+def find_out(answer): #get the parameter to be defined by user imput
+    answer='y'or'n'
+    for answer in input:
+
+        if answer=='n':
+            destination=random.choice(city_list)
+            input(f'Sorry to see that, would {destination} work? y/n')
+
+        elif answer=='y':
+            return destination
+        print(f'Awesome, we have determined you want to go to {destination}.')
+
+user_answer=find_out(input)
