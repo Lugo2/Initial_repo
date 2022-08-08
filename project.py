2ef540777a4a04437161f33317a826a523e02711
Mario Lugo <mlugo23699@gmail.com>
2022-08-08 13:26:16 -0700

    adding function that pick random location and function that asks user y/n about location choice

--- project.py
+++ project.py
@@ -1,40 +1,57 @@
 city_list=['Tijuana','Lima','Amsterdam','Cairo','Bangkok','Wagga Wagga']
-Tijuana_eats=['Sabor a Mi','Tacos el Gordo','Villa Marina Tijuana','Da Salvatore','Saketori-Ya']
+Tijuana_eats=['Tacos el Gordo','Villa Marina Tijuana','Da Salvatore','Saketori-Ya']
 Tijuana_transportation=['taxi','bus','car rental','bicycle']
-Tijuana_entertainment=['','','','','']
+Tijuana_entertainment=['Centro Cultural Tijuana','Mamut Cerveceria','Cinepolis','Malecon']
 
-Lima_eats=['','','','','']
-Lima_transportation=['','','','']
-Lima_entertainment=['','','','','']
+Lima_eats=['La Bodega de la Trattoria','Pizza Raul','Polleria Rokys','Chifa Oriental']
+Lima_transportation=['metro','car rental','taxi','on foot']
+Lima_entertainment=['Plaza Mayor','Lacro Museum','Morro Solar','Lima Golf Club']
 
-Amsterdam_eats=['','','','','']
-Amsterdam_transportation=['bicycle','','','']
-Amsterdam_entertainment=['','','','','']
+Amsterdam_eats=['','','','']
+Amsterdam_transportation=['','','','']
+Amsterdam_entertainment=['','','','']
 
-Cairo_eats=['','','','','']
+Cairo_eats=['','','','']
 Cairo_transportation=['','','','']
-Cairo_entertainment=['','','','','']
+Cairo_entertainment=['','','','']
 
-Bangkok_eats=['','','','','']
+Bangkok_eats=['','','','']
 Bangkok_transportation=['','','','']
-Bangkok_entertainment=['','','','','']
+Bangkok_entertainment=['','','','']
 
-Wagga_Wagga_eats=['','','','','']
+Wagga_Wagga_eats=['','','','']
 Wagga_Wagga_transportation=['','','','']
-Wagga_Wagga_entertainment=['','','','','']
+Wagga_Wagga_entertainment=['','','','']
 
-print('Hello, I am your firendly neighborhood day trip generator! Today we will be planning your next day trip. Lets go!')
+# def program_intoduction():
+#     print('Hello, I am your firendly neighborhood day trip generator! Today we will be planning your next day trip. Lets go!')
+# program_intoduction()
+
 import random
 
-def give_other(answer): #get the parameter to be defined by user imput
-    answer=='n'
-    for answer in input:
+def random_city_chooser(destination):
+    destination=random.choice(city_list)
+    return destination
 
-        if answer=='n':
-            destination=random.choice(city_list)
-            input(f'Sorry to see that, would {destination} work? y/n')
+random_city=random_city_chooser(city_list)
+
+def prompt_user_location():
+    answer=input(f'We have selected: {random_city}. Is this where you would like to go?y/n')
     return answer
 
-destination=random.choice(city_list)
-answer=input(f'I have chosen {destination} for you, is this where you would like to go? y/n')
-user_answer=give_other(answer)
+user_input=prompt_user_location()
+print(user_input)
+
+
+# answer=input(f'I have chosen {destination} for you, is this where you would like to go? y/n')
+
+
+
+# def new_city_chooser(no):#parameter will be user saying no to input prompt
+#     new_destination=random.choice(city_list)#trying to 
+#     return new_destination
+
+
+
+
+
